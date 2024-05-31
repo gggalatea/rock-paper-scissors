0564b9e2309cfcf1a0d40e9247bfd50e5083d2ec
galatea <157182004+gggalatea@users.noreply.github.com>
2024-06-01 09:15:51 +1200

    Add getHumanChoice function

--- script.js
+++ script.js
@@ -2,11 +2,27 @@
 
 function getComputerChoice() {
     const computerChoice = Math.floor(Math.random() * 3);
+
     if (computerChoice === 2) {
         return "rock";
     } else if (computerChoice === 1) {
         return "paper";
     } else {
         return "scissors";
+    }
+}
+
+function getHumanChoice() {
+    while (true) {
+        let humanChoice = prompt("Choose rock, paper or scissors!");
+        if (humanChoice.toLowerCase() === "rock") {
+            return "rock";
+        } else if (humanChoice.toLowerCase() === "paper") {
+            return "paper";
+        } else if (humanChoice.toLowerCase() === "scissors") {
+            return "scissors";
+        } else {
+            alert("You have entered rock, paper or scissors incorrectly! Please try again!")
+        }
     }
 }
