3c78e58d67e4528e8b0141875a6e1753ffea7e3f
galatea <157182004+gggalatea@users.noreply.github.com>
2024-06-02 06:58:49 +1200

    Add playGame function

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 "use strict";
 
-let humanScore = 0;
-let computerScore = 0;
-
 function getComputerChoice() {
     const computerChoice = Math.floor(Math.random() * 3);
 
@@ -26,28 +23,43 @@ function getHumanChoice() {
     }
 }
 
-function playRound(humanChoice, computerChoice) {
-    if (humanChoice === computerChoice) {
-        console.log(`You chose ${humanChoice[0].toUpperCase() + humanChoice.slice(1)}`);
-        console.log(`Computer chose ${computerChoice[0].toUpperCase() + computerChoice.slice(1)}`);
-        console.log(`Draw! You both chose ${humanChoice[0].toUpperCase() + humanChoice.slice(1)}`);
-        console.log(`Your score: ${humanScore}, Computer score: ${computerScore}`);
-    } else if ((humanChoice === "rock" && computerChoice === "scissors") || (humanChoice === "paper" && computerChoice === "rock") || (humanChoice === "scissors" && computerChoice === "paper")) {
-        console.log(`You chose ${humanChoice[0].toUpperCase() + humanChoice.slice(1)}`);
-        console.log(`Computer chose ${computerChoice[0].toUpperCase() + computerChoice.slice(1)}`);
-        console.log(`You win! ${humanChoice[0].toUpperCase() + humanChoice.slice(1)} beats ${computerChoice[0].toUpperCase() + computerChoice.slice(1)}`);
-        humanScore++;
-        console.log(`Your score: ${humanScore}, Computer score: ${computerScore}`);
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function playRound(humanChoice, computerChoice) {
+        if (humanChoice === computerChoice) {
+            console.log(`\nYou chose ${humanChoice[0].toUpperCase() + humanChoice.slice(1)}`);
+            console.log(`Computer chose ${computerChoice[0].toUpperCase() + computerChoice.slice(1)}`);
+            console.log(`Draw! You both chose ${humanChoice[0].toUpperCase() + humanChoice.slice(1)}`);
+            console.log(`Your score: ${humanScore}, Computer score: ${computerScore}`);
+        } else if ((humanChoice === "rock" && computerChoice === "scissors") || (humanChoice === "paper" && computerChoice === "rock") || (humanChoice === "scissors" && computerChoice === "paper")) {
+            console.log(`\nYou chose ${humanChoice[0].toUpperCase() + humanChoice.slice(1)}`);
+            console.log(`Computer chose ${computerChoice[0].toUpperCase() + computerChoice.slice(1)}`);
+            console.log(`You win! ${humanChoice[0].toUpperCase() + humanChoice.slice(1)} beats ${computerChoice[0].toUpperCase() + computerChoice.slice(1)}`);
+            humanScore++;
+            console.log(`Your score: ${humanScore}, Computer score: ${computerScore}`);
+        } else {
+            console.log(`\nYou chose ${humanChoice[0].toUpperCase() + humanChoice.slice(1)}`);
+            console.log(`Computer chose ${computerChoice[0].toUpperCase() + computerChoice.slice(1)}`);
+            console.log(`You lose! ${computerChoice[0].toUpperCase() + computerChoice.slice(1)} beats ${humanChoice[0].toUpperCase() + humanChoice.slice(1)}`);
+            computerScore++;
+            console.log(`Your score: ${humanScore}, Computer score: ${computerScore}`);
+        }
+    }
+
+    for (let i = 0; i < 5; i++) {
+        playRound(getHumanChoice(), getComputerChoice());
+    }
+
+    console.log(`\nYour final score: ${humanScore}, Computer final score: ${computerScore}`);
+    if (humanScore === computerScore) {
+        console.log("Draw!");
+    } else if (humanScore > computerScore) {
+        console.log("You win!");
     } else {
-        console.log(`You chose ${humanChoice[0].toUpperCase() + humanChoice.slice(1)}`);
-        console.log(`Computer chose ${computerChoice[0].toUpperCase() + computerChoice.slice(1)}`);
-        console.log(`You lose! ${computerChoice[0].toUpperCase() + computerChoice.slice(1)} beats ${humanChoice[0].toUpperCase() + humanChoice.slice(1)}`);
-        computerScore++;
-        console.log(`Your score: ${humanScore}, Computer score: ${computerScore}`);
+        console.log("You lose!");
     }
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
+playGame();
