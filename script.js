780e56fb677ecc34f2386f90c195f4fc9196a7cd
galatea <157182004+gggalatea@users.noreply.github.com>
2024-07-09 14:41:58 +1200

    Display score and create playNewGame function

--- script.js
+++ script.js
@@ -27,7 +27,15 @@ function getHumanChoice() {
 let playerScore = 0;
 let computerScore = 0;
 
-const btnsContainer = document.querySelector(".container");
+const score = document.querySelector(".score")
+
+score.textContent = `${playerScore} - ${computerScore}`;
+
+const newGameBtn = document.querySelector(".new-game");
+
+newGameBtn.style.display = "none";
+
+const btnsContainer = document.querySelector(".rps-container");
 const playerText = document.querySelector(".player-choice");
 const computerText = document.querySelector(".computer-choice");
 const result = document.querySelector(".result");
@@ -35,6 +43,10 @@ const result = document.querySelector(".result");
 btnsContainer.addEventListener("click", playRound);
 
 function playRound(e) {
+    if (playerScore === 5 || computerScore === 5) {
+        return;
+    }
+
     const playerChoice = e.target.dataset.playerChoice;
     if (!playerChoice) {
         return;
@@ -44,7 +56,7 @@ function playRound(e) {
     if (playerChoice === computerChoice) {
         playerText.textContent = `You chose: ${playerChoice}`;
         computerText.textContent = `Computer chose: ${computerChoice}`;
-        result.textContent = `Draw!`
+        result.textContent = `Draw!`;
     } else if ((playerChoice === "rock" && computerChoice === "scissors") || (playerChoice === "paper" && computerChoice === "rock") || (playerChoice === "scissors" && computerChoice === "paper")) {
         playerText.textContent = `You chose: ${playerChoice}`;
         computerText.textContent = `Computer chose: ${computerChoice}`;
@@ -56,13 +68,25 @@ function playRound(e) {
         result.textContent = `You lose!`;
         computerScore++;
     }
+    score.textContent = `${playerScore} - ${computerScore}`;
+
+    if (playerScore === 5) {
+        result.textContent = `You win the first to 5!`;
+        newGameBtn.style.display = "inline-block";
+    } else if (computerScore === 5) {
+        result.textContent = `You lose the first to 5!`;
+        newGameBtn.style.display = "inline-block";
+    }
 }
 
-console.log(`\nYour final score: ${playerScore}, Computer final score: ${computerScore}`);
-if (playerScore === computerScore) {
-    console.log("Draw!");
-} else if (playerScore > computerScore) {
-    console.log("You win!");
-} else {
-    console.log("You lose!");
+newGameBtn.addEventListener("click", playNewGame);
+
+function playNewGame() {
+    playerScore = 0;
+    computerScore = 0;
+    score.textContent = `${playerScore} - ${computerScore}`;
+    newGameBtn.style.display = "none";
+    playerText.textContent = ``;
+    computerText.textContent = ``;
+    result.textContent = ``;
 }
